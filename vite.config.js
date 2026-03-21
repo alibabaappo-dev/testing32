af0b3bfbbc0d8dcd0790a659e5b9a78bf22f3e36
alibabaappo-dev <alibabaappo@gmail.com>
2026-03-22 01:24:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true
+  },
   resolve: {
     dedupe: ['react', 'react-dom']
   }
